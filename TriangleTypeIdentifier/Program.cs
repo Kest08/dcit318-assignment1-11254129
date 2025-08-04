4570a7ae1862e67fa6d20a4be192c1caaf19403c
kabuteysamuel <stekabutey@st.ug.edu.gh>
2025-08-04 22:29:54 +0000

    Initial commit: added GradeCalculator, TicketPriceCalculator, and TriangleTypeIdentifier projects

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter side A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter side B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter side C: ");
+        int c = int.Parse(Console.ReadLine());
+
+        if (a == b && b == c)
+            Console.WriteLine("Triangle Type: Equilateral");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("Triangle Type: Isosceles");
+        else
+            Console.WriteLine("Triangle Type: Scalene");
+    }
+}
